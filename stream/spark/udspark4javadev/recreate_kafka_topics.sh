3fcc3f9dd4a95a3ec2df0d760a070672bfa86554
Sviatoslav Zimin <slzdev@gmail.com>
2020-04-27 08:57:22 +0200

    kafkajson module: schema, kafka rw ok

--- stream/spark/udspark4javadev/recreate_kafka_topics.sh
+++ stream/spark/udspark4javadev/recreate_kafka_topics.sh
@@ -2,7 +2,7 @@
 
 btopic0=viewrecords
 btopic1=recresults
-#btopic2=std-wattsight
+btopic2=json_incoming
 
 npartitions=1
 repl_factor=1
@@ -12,12 +12,12 @@ bootsrv=localhost:9092
 echo deleting topics..
 kafka-topics.sh --bootstrap-server ${bootsrv}  --delete --topic ${btopic0}
 kafka-topics.sh --bootstrap-server ${bootsrv}  --delete --topic ${btopic1}
-#kafka-topics.sh --bootstrap-server ${bootsrv}  --delete --topic ${btopic2}
+kafka-topics.sh --bootstrap-server ${bootsrv}  --delete --topic ${btopic2}
 
 echo creating topics..
 kafka-topics.sh --bootstrap-server ${bootsrv}  --partitions ${npartitions} --replication-factor ${repl_factor}  --create --topic ${btopic0}
 kafka-topics.sh --bootstrap-server ${bootsrv}  --partitions ${npartitions} --replication-factor ${repl_factor}  --create --topic ${btopic1}
-#kafka-topics.sh --bootstrap-server ${bootsrv}  --partitions ${npartitions} --replication-factor ${repl_factor}  --create --topic ${btopic2}
+kafka-topics.sh --bootstrap-server ${bootsrv}  --partitions ${npartitions} --replication-factor ${repl_factor}  --create --topic ${btopic2}
 
 echo "now topics .."
 kafka-topics.sh --bootstrap-server ${bootsrv} --list 
