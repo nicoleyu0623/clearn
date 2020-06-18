1abd68089f40d6c61231ba5e0f34ae2ee01f7346
sviatoslav zimine <slzdev@gmail.com>
2020-06-18 02:29:01 +0200

    intellij tricks wip ch 1-6

---
+++ java8/core/corelng/Intellij/src/main/java/navigation/DuplicatingAndDeletingLine.java
@@ -0,0 +1,9 @@
+package navigation;
+
+public class DuplicatingAndDeletingLine {
+    void methodA() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
---
+++ java8/core/corelng/Intellij/src/test/java/codecompletion/StaticImportCompletionTest.java
@@ -0,0 +1,14 @@
+package codecompletion;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StaticImportCompletionTest {
+
+    @Test
+    public void testMethod() throws Exception{
+       Assertions.assertSame("1","1");
+    }
+
+}
