463ac80b725fc22532c1d294baf3c22ac4bd39ad
sviatoslav zimine <slzdev@gmail.com>
2021-02-07 03:31:44 +0100

    groovy tutorialspoint ok

---
+++ java8/core/corelng/FuncProg/src/test/java/org/slzdevsnp/io/TestFileReadWrite.java
@@ -0,0 +1,83 @@
+package org.slzdevsnp.io;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+public class TestFileReadWrite {
+
+    @Test
+    void givenFileModifiedWithinPeriodRecreate() throws IOException {
+        char SEPARATOR = ',';
+        String fname="/tmp/config.csv";
+        File file = new File(fname);
+
+        boolean isModifiedWithinWindow = isFileModifiedWithinSecond(fname,50);
+        FileWriter fileWriter = new FileWriter(fname, isModifiedWithinWindow);
+        String[] params = {"0","ccdd"};
+        writeLine(fileWriter,params,SEPARATOR);
+        fileWriter.flush();
+        fileWriter.close();
+
+        assertThat(file.exists(), is(true));
+
+    }
+
+    /**
+     * checks if a file was last modified within a number of seconds  from now
+     */
+    public boolean isFileModifiedWithinSecond(String filename, int nseconds){
+        File file = new File(filename);
+        long lstmod = file.lastModified(); //in system zone
+        LocalDateTime now = LocalDateTime.now();
+        ZoneOffset zoneOffset = ZoneId.systemDefault().getRules().getOffset(now);
+        long tsnow =  now.toInstant(zoneOffset).toEpochMilli();
+        //System.out.println("now:"+tsnow+" lastmod:"+ lstmod + " diff:"+String.valueOf(tsnow-lstmod));
+        boolean isModifiedWithinWindow = false;
+        if (tsnow-lstmod < nseconds*1000){
+            isModifiedWithinWindow = true;
+            //System.out.println("yes within window of modification");
+        }
+        return isModifiedWithinWindow ;
+    }
+    /**
+     writes a line from an array of string words to a FileWriter
+     */
+    public void writeLine(FileWriter writer, String[] params, char separator) throws IOException {
+        boolean firstParam = true;
+        StringBuilder stringBuilder = new StringBuilder();
+        String param = "";
+        for (int i = 0; i < params.length; i++)
+        {
+            //get param
+            param = params[i];
+            //log.info(param);
+            //if the first param in the line, separator is not needed
+            if (!firstParam)
+            {
+                stringBuilder.append(separator);
+            }
+            stringBuilder.append(param);
+            firstParam = false;
+        }
+        //prepare file to next line
+        stringBuilder.append("\n");
+
+        //add to file the line
+        //log.info(stringBuilder.toString());
+        writer.append(stringBuilder.toString());
+    }
+
+
+}
