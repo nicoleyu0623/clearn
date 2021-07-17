c586bc13cecc07028304d54c55a7b03ad9b104f3
sviatoslav zimine <sviatoslav.zimine@alpiq.com>
2021-07-17 02:59:27 +0200

    concurreny2patterns wip

---
+++ java8/concurrency/apply_concurrency2common_java_patterns/lab/concurrency2patterns/01-understand/src/main/java/org/paumard/runnable/model/LongWrapper.java
@@ -0,0 +1,24 @@
+package org.paumard.runnable.model;
+
+public class LongWrapper {
+
+	private Object key = new Object();
+	private long l;
+
+	public LongWrapper(long l) {
+		this.l = l;
+	}
+
+	public long getValue() {
+		synchronized (key) {
+			return l;
+		}
+	}
+
+	public void incrementValue() {
+		synchronized (key) {
+			l+=1; //this is a read and write operation
+		}
+		//l+=1;
+	}
+}
